19e3d513e0c8460fa90a33b73919a55fb737c3a5
prudhvi-hack <gadupudiprudhvi@gmail.com>
2020-03-04 23:17:43 +0530

    commit after formatting the rendered text

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,6 @@
 #todos_controller.rb
 class TodosController < ApplicationController
   def index
-    render plain: Todo.all.to_a
+    render plain: Todo.all.order(:due_date).map { |todo| todo.to_displayable_string }.join("\n")
   end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,7 @@
 class Todo < ActiveRecord::Base
+  def to_displayable_string
+    is_completed = completed ? "[X]" : "[ ]"
+    date = due_date.to_s(:long)
+    "#{id}. #{date} #{todo_text} #{is_completed} "
+  end
 end
