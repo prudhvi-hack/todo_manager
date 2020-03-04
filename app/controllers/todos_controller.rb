71db186998284d445677eb665fba022278286f2b
prudhvi-hack <gadupudiprudhvi@gmail.com>
2020-03-05 00:37:29 +0530

    modified http verb as post for create route

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,7 @@
 #todos_controller.rb
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.all.order(:due_date).map { |todo| todo.to_displayable_string }.join("\n")
   end
