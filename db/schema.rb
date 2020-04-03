e7c8f57d26b763a4507e568c5e67e5d7b749721e
prudhvi-hack <gadupudiprudhvi@gmail.com>
2020-04-03 16:29:37 +0530

    changed typo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_200252) do
+ActiveRecord::Schema.define(version: 2020_04_03_095121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2020_03_04_200252) do
 
   create_table "users", force: :cascade do |t|
     t.text "name"
-    t.text "gmail"
+    t.text "email"
     t.text "password"
   end
 
