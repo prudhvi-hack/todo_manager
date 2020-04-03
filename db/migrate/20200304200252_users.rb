6cfe8641652adc3d4e6c7bd24ff6f924b2d863df
prudhvi-hack <gadupudiprudhvi@gmail.com>
2020-04-03 16:05:26 +0530

    changed typo

--- db/migrate/20200304200252_users.rb
+++ db/migrate/20200304200252_users.rb
@@ -2,7 +2,7 @@ class Users < ActiveRecord::Migration[6.0]
   def change
     create_table(:users) do |t|
       t.column :name, :text
-      t.column :gmail, :text
+      t.column :email, :text
       t.column :password, :text
     end
   end
