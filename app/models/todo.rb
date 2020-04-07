229d9e71295afd34dd79a93d5394838286e33f29
prudhvi-hack <gadupudiprudhvi@gmail.com>
2020-04-07 21:39:58 +0530

    moved all logic to model

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -14,16 +14,13 @@ class Todo < ActiveRecord::Base
   def self.due_today
     where("due_date = ?", Date.today)
   end
-  def self.Overdue
-    where("due_date < ?", Date.today)
+  def self.overdue
+    where("due_date < ? and completed=?", Date.today, false)
   end
   def self.due_later
     where("due_date > ?", Date.today)
   end
   def self.completed
     where("completed = ?", true)
-  end
-  def self.not_completed
-    where("completed = ?", false)
   end
 end
