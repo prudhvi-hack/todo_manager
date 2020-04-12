e0e7d5e18a76a0e7ea7ec082def470962db31ec0
prudhvi-hack <gadupudiprudhvi@gmail.com>
2020-04-13 00:03:18 +0530

    added signout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get "/", to: "home#index"
   get "/signin", to: "sessions#new", as: :new_sessions
   post "/signin", to: "sessions#create", as: :sessions
+  delete "/signout", to: "sessions#destroy", as: :destroy_session
 end
