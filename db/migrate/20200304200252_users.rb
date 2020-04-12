b0e28dffed7cc135e88d02f203e853c05003963c
prudhvi-hack <gadupudiprudhvi@gmail.com>
2020-04-12 15:32:29 +0530

    create required migrations

--- db/migrate/20200304200252_users.rb
+++
@@ -1,9 +0,0 @@
-class Users < ActiveRecord::Migration[6.0]
-  def change
-    create_table(:users) do |t|
-      t.column :name, :text
-      t.column :email, :text
-      t.column :password, :text
-    end
-  end
-end
